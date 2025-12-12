244a5e094a653e9db92d9df360e01a943973f3f9
cygubicko <cygu@aims.ac.za>
2025-12-12 12:41:29 +0300

    Safe start

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 docker rm -f no-code-pycaret 2>/dev/null || true
 docker rm -f no-code-app       2>/dev/null || true
 
-docker compose -p no-code-app:latest pull
-docker compose -p no-code-app down --remove-orphans
+docker compose -p no-code-app pull
+docker compose -p no-code-app down --remove-orphans  2>/dev/null
 docker compose -p no-code-app up -d
 
